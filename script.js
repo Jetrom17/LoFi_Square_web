cec5e320fc1e493c0ecbd80a965023112145f3cf
Jeiel Lima Miranda <github.esvlc@slmail.me>
2024-08-15 20:14:23 -0300

    Update script.js

--- script.js
+++ script.js
@@ -4,72 +4,78 @@ const playButton = document.getElementById('play');
 const nextButton = document.getElementById('next');
 const prevButton = document.getElementById('prev');
 const trackName = document.getElementById('track-name');
+const volumeSlider = document.getElementById('volume-slider');
 
 // Lista das faixas
 const tracks = [
-  'http://radio.stereoscenic.com/ama-h',
-  'http://radio.stereoscenic.com/ama-s',
-  'http://163.172.169.217:80/asp-h',
-  'http://radio.stereoscenic.com:80/am-h.mp3',
-  'http://163.172.169.217:80/asp-s',
-  'http://radio.stereoscenic.com:80/am-s.mp3',
-  'http://163.172.169.217:80/asp-l',
-  'http://radio.stereoscenic.com:80/am-l.mp3',
+    'http://radio.stereoscenic.com/ama-h',
+    'http://radio.stereoscenic.com/ama-s',
+    'http://163.172.169.217:80/asp-h',
+    'http://radio.stereoscenic.com:80/am-h.mp3',
+    'http://163.172.169.217:80/asp-s',
+    'http://radio.stereoscenic.com:80/am-s.mp3',
+    'http://163.172.169.217:80/asp-l',
+    'http://radio.stereoscenic.com:80/am-l.mp3',
 ];
 
 let currentTrackIndex = 0;
 
 // Função para reproduzir a faixa
 function playTrack(index) {
-  audio.src = tracks[index];
-  audio.play().then(() => {
-    trackName.textContent = `Playing ${index + 1}`;
-    playButton.classList.remove('fa-play');
-    playButton.classList.add('fa-pause');
-  }).catch((error) => {
-    console.error('Erro ao reproduzir a faixa:', error);
-  });
+    audio.src = tracks[index];
+    audio.play().then(() => {
+        trackName.textContent = `Playing ${index + 1}`;
+        playButton.classList.remove('fa-play');
+        playButton.classList.add('fa-pause');
+    }).catch((error) => {
+        console.error('Erro ao reproduzir a faixa:', error);
+    });
 }
 
 // Função para pausar a faixa
 function pauseTrack() {
-  audio.pause();
-  trackName.textContent = 'Pausado';
-  playButton.classList.remove('fa-pause');
-  playButton.classList.add('fa-play');
+    audio.pause();
+    trackName.textContent = 'Pausado';
+    playButton.classList.remove('fa-pause');
+    playButton.classList.add('fa-play');
 }
 
 // Manipuladores de eventos
 playButton.addEventListener('click', () => {
-  if (audio.paused) {
-    playTrack(currentTrackIndex);
-  } else {
-    pauseTrack();
-  }
+    if (audio.paused) {
+        playTrack(currentTrackIndex);
+    } else {
+        pauseTrack();
+    }
 });
 
 nextButton.addEventListener('click', () => {
-  currentTrackIndex = (currentTrackIndex + 1) % tracks.length;
-  playTrack(currentTrackIndex);
+    currentTrackIndex = (currentTrackIndex + 1) % tracks.length;
+    playTrack(currentTrackIndex);
 });
 
 prevButton.addEventListener('click', () => {
-  currentTrackIndex = (currentTrackIndex - 1 + tracks.length) % tracks.length;
-  playTrack(currentTrackIndex);
+    currentTrackIndex = (currentTrackIndex - 1 + tracks.length) % tracks.length;
+    playTrack(currentTrackIndex);
+});
+
+// Controle de volume
+volumeSlider.addEventListener('input', () => {
+    audio.volume = volumeSlider.value / 100;
 });
 
 // Dialog
 document.addEventListener('DOMContentLoaded', () => {
-  setTimeout(() => {
-    const dialog = document.getElementById('download-dialog');
-    dialog.classList.add('show');
-
-    // Role suavemente para o modal
     setTimeout(() => {
-      dialog.scrollIntoView({
-        behavior: 'smooth',
-        block: 'start'
-      });
-    }, 500); // Tempo para garantir que o modal já tenha começado a aparecer
-  }, 3000); // Tempo antes do modal aparecer
+        const dialog = document.getElementById('download-dialog');
+        dialog.classList.add('show');
+
+        // Role suavemente para o modal
+        setTimeout(() => {
+            dialog.scrollIntoView({
+                behavior: 'smooth',
+                block: 'start'
+            });
+        }, 0); // Tempo para garantir que o modal já tenha começado a aparecer
+    }, 15000); // Tempo antes do modal aparecer
 });
